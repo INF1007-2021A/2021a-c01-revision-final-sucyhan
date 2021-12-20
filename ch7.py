6ed7eea9f89339561db280a3f3e5dc50592caec8
sucyhan <90001453+sucyhan@users.noreply.github.com>
2021-12-20 14:44:07 -0500

    FINI

--- ch7.py
+++ ch7.py
@@ -10,15 +10,18 @@ from twitch_bot import *
 def build_say_hi_callback(bot, message):
 	# TODO: Créer et retourner une fonction qui prend un paramètre (ignoré).
 	#       Cette fonction envoie `message` dans le chat à l'aide de la méthode `send_privmsg` du paramètre `bot`.
-	pass
+	def callback(*arg):
+		bot.send_privmsg(message)
+		return callback
+
 
 def run_ch7_example():
 	bot = TwitchBot("logs")
 	# TODO: Construire le callback avec le bot et un message de votre choix.
-	callback = ...
+	callback = build_say_hi_callback(bot,"Wee")
 	# TODO: Enregister le callback sous la commande "say_hi".
-	bot.register_command(...)
+	bot.register_command("say_hi",callback)
 	# TODO: Mettre votre jeton (incluant le "oauth:") et le nom du compte Twitch associé.
-	bot.connect_and_join("oauth:...", "...", "chosson")
+	bot.connect_and_join("oauth:59wnylg75xve5lt4ua07609pidlv7i", "sxue_", "sxue_")
 	bot.run()
 
--- ch8.py
+++ ch8.py
@@ -19,21 +19,21 @@ def load_config(filename):
 	config = configparser.ConfigParser()
 	config.read(filename)
 	# TODO: Extraire le nom du channel, le nom du compte et le mot de passe (jeton) dans les variables.
-	channel = ...
-	bot_nickname = ...
-	bot_password = ...
+	channel = config["chat"]["channel"]
+	bot_nickname = config["login"]["account_name"]
+	bot_password = config["login"]["account_oath_token"]
 	return ConfigInfo(bot_nickname, bot_password, channel)
 
 def load_quotes(filename):
 	# TODO: Charger le contenu du fichier JSON des citations.
-	pass
+	return json.load(open(filename, "r", encoding="UTF-8"))
 
 def build_quotes_callback(bot, quotes):
 	def callback(*args):
 		# TODO: Choisir une catégorie au hasard.
-		random_category = ...
+		random_category = random.choice(tuple(quotes.keys()))
 		# TODO: Choisir une citation au hasard dans la catégorie.
-		random_quote = ...
+		random_quote = random.choice(quotes[random_category])
 		bot.send_privmsg(random_quote)
 	return callback
 
